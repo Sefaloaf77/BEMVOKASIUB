7df940d0cec3d0f836a811038d9e695915a2edb3
Sefalo AF <sefaloaf@gmail.com>
2022-10-31 19:44:18 +0700

    navbar footer

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<?php include('./partials/head.php') ?>
+
+
+<body>
+    <?php include('./partials/navbar.php') ?>
+
+    <!-- isi -->
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- isi -->
+    
+    <?php include('./partials/footer.php') ?>
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ partials/navbar.php
@@ -0,0 +1,47 @@
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-light shadow">
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="#">
+            <img src="assets/img/logo-navbar.svg" alt="logo" class="me-3" height="50px">
+        </a>
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-dark">
+                <li class="nav-item">
+                    <a class="nav-link active home" href="index.php">Beranda</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Kabinet
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Presidan dan Wakil</a></li>
+                        <li><a class="dropdown-item" href="#">Menteri Koordinator</a></li>
+                        <li><a class="dropdown-item" href="#">BJMO</a></li>
+                        <li><a class="dropdown-item" href="#">Biro Administrasi</a></li>
+                        <li><a class="dropdown-item" href="#">Biro Keuangan</a></li>
+                        <li><a class="dropdown-item" href="#">Biro PUSKOMINFO</a></li>
+                        <li><a class="dropdown-item" href="#">Kementerian PSDM</a></li>
+                        <li><a class="dropdown-item" href="#">Kementerian PORA</a></li>
+                        <li><a class="dropdown-item" href="#">Kementerian ADVOKESMA</a></li>
+                        <li><a class="dropdown-item" href="#">Kementerian SOSMA</a></li>
+                        <li><a class="dropdown-item" href="#">Kementerian Perhubungan</a></li>
+                        <li><a class="dropdown-item" href="#">Kementerian KASTRAT</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link competition" href="#">Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link merch" href="#">Kontak</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link merch" href="#">ORMAWA</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- navbar -->
