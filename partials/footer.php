8327e8a3507910327168982733cb088db4e54b5b
Sefalo AF <sefaloaf@gmail.com>
2022-11-13 19:55:43 +0700

    halaman beranda

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,5 @@
 <!-- footer -->
-<div class="bg-footer"><img src="assets/img/bg-bottom.svg" alt="" width="100%" loading="lazy"></div>
+
 <footer class="py-4 px-2">
     <div class="container">
         <div class="row pt-3 pb-5">
@@ -38,9 +38,31 @@
             <div class="col-md-5 col-lg-5 me-0 mt-3 mt-md-0 lokasi">
                 <h4 class="text-white">Lokasi</h4>
                 <p class="text-white">Jl. Dieng Atas, Sumberjo, Kalisongo, Kec. Dau, Kabupaten Malang, Jawa Timur 65151</p>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6227.167080068664!2d112.58990478558101!3d-7.96833811633242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7883389e07f327%3A0x9f3648ae9216b820!2sLab%20dan%20Industri%20Kreatif%20Fakultas%20Vokasi%20UB!5e0!3m2!1sid!2sid!4v1667177880336!5m2!1sid!2sid"style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6227.167080068664!2d112.58990478558101!3d-7.96833811633242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7883389e07f327%3A0x9f3648ae9216b820!2sLab%20dan%20Industri%20Kreatif%20Fakultas%20Vokasi%20UB!5e0!3m2!1sid!2sid!4v1667177880336!5m2!1sid!2sid" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
 </footer>
 <!-- footer -->
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+    AOS.init({
+        once: true,
+        duration: 1000,
+    });
+</script>
+
+<!-- loading -->
+<script>
+    var preloader = document.getElementById("loading");
+
+    function myFunction() {
+        preloader.style.display = "none";
+    }
+</script>
