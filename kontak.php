21fa1db78dcb48bf6ac8bc8a6eeba950ebf9979c
nisrinaawn <nwanadaa@gmail.com>
2022-12-30 11:44:58 +0700

    kontak

--- kontak.php
+++ kontak.php
@@ -1,15 +1,228 @@
 <!doctype html>
 <html lang="en">
 <?php include('./partials/head.php') ?>
-
+<link rel="stylesheet" href="assets/css/kontak.css">
 
 <body>
     <?php include('./partials/navbar.php') ?>
-
+    <section class="top">
+<img src="assets/img/bg-top.svg" alt="">
+    </section>
     <!-- isi -->
 
+    <div class="container">
+        <div class="filosofi-logo">
+            <h1 class="title-filosofi">Hubungi Kami Disini</h1>
+        </div>
 
 
+    <div class="team">
+            
+        <div class="team_box">
+            <div class="profile">
+                <img src="assets/img/Rectangle220.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Media Partner</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Rectangle 321.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Advokesma Care</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Rectangle225.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Pengambilan<br>Sertifikat PKKMB</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="team">
+        <div class="team_box">
+            <div class="profile">
+                <img src="assets/img/Rectangle222.png">
+                <div class="info">
+                    <h2 class="name">Email</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Rectangle223.png">
+                <div class="info">
+                    <h2 class="name">Instagram</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Rectangle224.png">
+                <div class="info">
+                    <h2 class="name">Spotify</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="team">
+        <div class="team_box">
+            <div class="profile">
+                <img src="assets/img/Rectangle221.png">
+                <div class="info">
+                    <h2 class="name">Twitter</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Rectangle226.png">
+                <div class="info">
+                    <h2 class="name">Youtube</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Component 1 (2).png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>BJMO</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="team">
+        <div class="team_box">
+            <div class="profile">
+                <img src="assets/img/Component31.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Biro Keuangan</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Component32.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Biro Administrasi</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Component33.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Biro Puskominfo</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="team">
+        <div class="team_box">
+            <div class="profile">
+                <img src="assets/img/Component30.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Kementerian<br>PSDM</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Component20.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Kementerian<br>PORA</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Component21.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Kementerian<br>SOSMA</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="team">
+        <div class="team_box">
+            <div class="profile">
+                <img src="assets/img/Component22.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Kementerian<br>ADVOKESMA</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Component23.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Kementerian<br>Perhubungan</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="profile">
+                <img src="assets/img/Component24.png">
+                <div class="info">
+                    <h2 class="name">Narahubung<br>Kementerian<br>KASTRAT</h2>
+                    <div class="team_icon">
+                        <i class="fa-sharp fa-solid fa-link"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+<h1></h1>
 
 
 
@@ -20,8 +233,12 @@
 
 
     <!-- isi -->
-
+    <section class="bottom">
+            <img src="assets/img/bg-bottom.svg">
+    </section>
     <?php include('./partials/footer.php') ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" integrity="sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
 
